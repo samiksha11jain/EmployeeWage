438634cc6dd8245f036abfcdaf413cd1c815ad02
“samiksha11jain” <samiksha11jain@gmail.com>
2022-01-15 22:32:43 +0530

    adding PartTime ProgramUC3

---
+++ NewFolder2/PartTimeProgram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage.NewFolder2
+{
+    class PartTimeProgram
+    {
+        static void Main(string[] args)
+        {
+            //Constants
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else
+            {
+                empHrs = 8;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
+        }
+    }
+}
