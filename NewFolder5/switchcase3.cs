4f855dc14ac79594bf70da01df37c4a45812fc54
“samiksha11jain” <samiksha11jain@gmail.com>
2022-01-15 23:52:26 +0530

    adding SwitchCase3 programUC6

---
+++ NewFolder5/switchcase3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage.NewFolder5
+{
+    class switchcase3
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WRKING_DAYS = 2;
+        public const int MAX_HRS_IN_MONTH = 10;
+        static void Main(string[] args)
+        {
+            //variables
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDys = 0;
+            //computation
+            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WRKING_DAYS)
+            {
+                tptalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                        case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                        default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days#:" + totalWorkingDays + "Emp Hrs : " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage : " + totalEmpWge);
+
+
+        }
+        
+        
+             
+               
+            
+
+
+            
+    }
+}
