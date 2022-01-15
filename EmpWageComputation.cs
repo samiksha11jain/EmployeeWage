f824fef55ce84ea00c19a203fa78220fd14a7ee8
“samiksha11jain” <samiksha11jain@gmail.com>
2022-01-15 21:25:53 +0530

    add EmpWageprogramUC1

---
+++ EmpWageComputation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    class EmpWageComputation
+    {
+        static void Main(string[] args)
+        {
+            //Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME )
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");            }
+        }
+    }
+}
